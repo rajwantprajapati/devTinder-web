f78747c469aca66ea5152e4958cd7daa6296aca5
Rajwant Prajapati <rajwantprajapati5791@gmail.com>
2026-01-11 19:13:38 +0530

    feat: Implmement connections page

--- src/Pages/Connections/Connections.jsx
+++ src/Pages/Connections/Connections.jsx
@@ -28,9 +28,41 @@ const Connections = () => {
 
   console.log("connections: ", connections);
 
+  if (connections?.length === 0) {
+    return <h1>No connection found!</h1>;
+  }
+
   return (
-    <div>
-      <h1>Connections</h1>
+    <div className="my-10">
+      <h1 className="text-bold text-2xl ml-5">Connections</h1>
+
+      <div className="flex flex-col gap-4  m-5">
+        {connections.map(
+          ({ _id, firstName, lastName, photoUrl, age, gender, about }) => (
+            <div
+              key={_id}
+              className="card card-side bg-base-300 shadow-sm w-3/4 mx-auto"
+            >
+              <figure className="p-2">
+                <img
+                  src={photoUrl}
+                  alt={`${firstName}-photo`}
+                  className="h-30 w-30 rounded-full"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">
+                  {firstName} {lastName}
+                </h2>
+                <p className="grow-0">
+                  {[age, gender].filter(Boolean).join(", ")}
+                </p>
+                <p className="grow-0">{about}</p>
+              </div>
+            </div>
+          ),
+        )}
+      </div>
     </div>
   );
 };
